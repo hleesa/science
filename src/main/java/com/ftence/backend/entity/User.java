db057ab7c660ea78837ab11b21c8bcdb6ada635c
salee2 <lshhi100@gmail.com>
2023-02-17 18:53:01 +0900

    [feat] seoul42 login

--- src/main/java/com/ftence/backend/entity/User.java
+++ src/main/java/com/ftence/backend/entity/User.java
@@ -1,6 +1,7 @@
 package com.ftence.backend.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -18,8 +19,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "intra_id")
+
+    @Column(nullable = false)
     private String intraId;
+    @Column
+    private String picture;
 
     private double level;
 
@@ -28,5 +32,27 @@ public class User {
 
     @Column(name = "access_token")
     private String accessToken;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String intraId, String picture, Role role) {
+        this.intraId = intraId;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String intraId, String picture) {
+        this.intraId = intraId;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
 
 }
--- src/main/java/com/ftence/backend/oauth2/SecurityConfig.java
+++ src/main/java/com/ftence/backend/oauth2/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ftence.backend.oauth2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,17 +18,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
 
-    @Autowired
-    private CustomAuthenticationProvider authProvider;
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(authProvider);
-        return authenticationManagerBuilder.build();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,29 +31,34 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .logout()
+                .logoutUrl("/")
+                .and()
                 .oauth2Login()
-                .loginPage("/oauth_login")
-                .authorizationEndpoint()
-                .baseUri("/oauth2/authorize-client")
-                .authorizationRequestRepository(authorizationRequestRepository())
-                .and()
-                .tokenEndpoint()
-                .accessTokenResponseClient(accessTokenResponseClient())
-                .and()
-                .defaultSuccessUrl("/loginSuccess", true)
-                .failureUrl("/loginFailure");
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+//                .loginPage("/oauth_login")
+//                .authorizationEndpoint()
+//                .baseUri("/oauth2/authorize-client")
+//                .authorizationRequestRepository(authorizationRequestRepository())
+//                .and()
+//                .tokenEndpoint()
+//                .accessTokenResponseClient(accessTokenResponseClient())
+//                .and()
+//                .defaultSuccessUrl("/loginSuccess", true)
+//                .failureUrl("/loginFailure");
         return http.build();
     }
 
-    @Bean
-    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
-        return new HttpSessionOAuth2AuthorizationRequestRepository();
-    }
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
-        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
-        return accessTokenResponseClient;
-    }
+//    @Bean
+//    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+//        return new HttpSessionOAuth2AuthorizationRequestRepository();
+//    }
+//
+//    @Bean
+//    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+//        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
+//        return accessTokenResponseClient;
+//    }
 
 }
